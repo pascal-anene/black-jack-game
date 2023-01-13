200beae0fbc1995825c44d4e8dec80176cdbf303
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-13 20:57:19 +0100

    refactored shuffle function

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ for suit in suits:
    for rank in ranks:
     cards.append([suit, rank])
 
-random.shuffle(cards)
+def shuffle():
+   random.shuffle(cards)
 
 card = cards.pop()
-print(card)
+
+shuffle()
+print(cards)
 
 
 
