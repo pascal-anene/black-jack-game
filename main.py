b1a849efa1fbdfdb5d7e0fc50b6bf306d26ccbc6
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-13 21:31:57 +0100

    assigning values for A, J, Q and K cards

--- main.py
+++ main.py
@@ -32,7 +32,16 @@ cards_dealt = deal(2)
 
 card = cards_dealt[0]
 rank = card[1]
-print(card)
+
+if rank == "A":
+   value = 11
+elif rank == "J" or rank == "Q" or rank == "K":
+   value = 10
+else:
+   value = rank
+
+
+print(rank, value)
 
 
 
