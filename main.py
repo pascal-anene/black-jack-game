b18812f241b1df06ebead1d4f9a9a64535949bf3
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-13 20:43:17 +0100

    printing all cards in the deck

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ suits = ["spades", "clubs", "hearts", "diamonds"]
 
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-suits.append("snakes")
-
-
 
 for suit in suits:
-    print(suit)
+   for rank in ranks:
+    print([suit, rank])
+
+
+
 
 
 
