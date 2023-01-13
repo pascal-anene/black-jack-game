fdf1607a778ddbcf0720c4853c28cb3f52ad3421
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-13 22:55:14 +0100

    adding self to enable reference to instances or objects

--- main.py
+++ main.py
@@ -7,33 +7,34 @@
 import random
 
 class Deck:
-   cards = []
+   def __init__(self) -> None:
+      cards = []
 
-   suits = ["spades", "clubs", "hearts", "diamonds"]
+      suits = ["spades", "clubs", "hearts", "diamonds"]
 
-   ranks = [{"rank": "A", "value": 11},
-         {"rank": "2", "value": 2},
-         {"rank": "3", "value": 3},
-         {"rank": "4", "value": 4},
-         {"rank": "5", "value": 5},
-         {"rank": "6", "value": 6},
-         {"rank": "7", "value": 7},
-         {"rank": "8", "value": 8},
-         {"rank": "9", "value": 9},
-         {"rank": "10", "value": 10},
-         {"rank": "J", "value": 10},
-         {"rank": "Q", "value": 10},
-         {"rank": "K", "value": 10}]
+      ranks = [{"rank": "A", "value": 11},
+            {"rank": "2", "value": 2},
+            {"rank": "3", "value": 3},
+            {"rank": "4", "value": 4},
+            {"rank": "5", "value": 5},
+            {"rank": "6", "value": 6},
+            {"rank": "7", "value": 7},
+            {"rank": "8", "value": 8},
+            {"rank": "9", "value": 9},
+            {"rank": "10", "value": 10},
+            {"rank": "J", "value": 10},
+            {"rank": "Q", "value": 10},
+            {"rank": "K", "value": 10}]
 
 
-   for suit in suits:
-      for rank in ranks:
-      cards.append([suit, rank])
+      for suit in suits:
+         for rank in ranks:
+            cards.append([suit, rank])
 
-   def shuffle():
+   def shuffle(self):
       random.shuffle(cards)
 
-   def deal(number):
+   def deal(self, number):
       cards_dealt = []
       for x in range(number):
          card = cards.pop() # the value card cannot be accessed outside function context
