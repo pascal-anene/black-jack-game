573d5ffb97f6005e57f764582f9160293a16c9ca
Pascal Anene <pascalanene@Pascals-MacBook-Pro.local>
2023-01-13 20:51:02 +0100

    adding shuffling function to card deck

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 """
 
+import random
+
 cards = []
 
 suits = ["spades", "clubs", "hearts", "diamonds"]
@@ -13,8 +15,10 @@ ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 for suit in suits:
    for rank in ranks:
-    print([suit, rank])
+    cards.append([suit, rank])
 
+random.shuffle(cards)
+print(cards)
 
 
 
